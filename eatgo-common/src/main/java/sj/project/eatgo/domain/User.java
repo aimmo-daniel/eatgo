429342187a5a273832c694a9d6596563e455174a
tkdwl <sangjin0309>
2020-01-17 17:16:15 +0900

    사용자 관리

---
+++ eatgo-common/src/main/java/sj/project/eatgo/domain/User.java
@@ -0,0 +1,41 @@
+package sj.project.eatgo.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id @GeneratedValue
+    Long id;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String name;
+
+    @NotNull
+    private Long level;
+
+    public boolean isAdmin() {
+        return level >= 100;
+    }
+
+    public boolean isActive() {
+        return level > 0;
+    }
+
+    public void deactivate() {
+        level = 0L;
+    }
+}
